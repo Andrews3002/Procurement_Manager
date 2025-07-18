3c9aba6ae00f0f0eddd5710b13e190ce5388ddca
Andrews3002 <alexangeloandrews@gmail.com>
2025-07-18 15:19:45 -0400

    adding the render files

---
+++ renderer/renderer.js
@@ -0,0 +1,47 @@
+const homePage = document.querySelector('#HomePage')
+const scrapePage = document.querySelector('#ScrapePage')
+const supplierPage = document.querySelector('#SupplierPage')
+const requestPage = document.querySelector('#RequestPage')
+
+const scrapePageButton = document.querySelector('#ScrapePageButton')
+const supplierPageButton = document.querySelector('#SupplierPageButton')
+const requestPageButton = document.querySelector('#RequestPageButton')
+
+const scrapeHomeButton = document.querySelector('#ScrapeHomeButton')
+const supplierHomeButton = document.querySelector('#SupplierHomeButton')
+const requestHomeButton = document.querySelector('#RequestHomeButton')
+
+function openScrapePage(){
+    homePage.style.display = 'none'
+    scrapePage.style.display = 'flex'
+}
+
+function openSupplierPage(){
+    homePage.style.display = 'none'
+    supplierPage.style.display = 'flex'
+}
+
+function openRequestPage(){
+    homePage.style.display = 'none'
+    requestPage.style.display = 'flex'
+}
+
+function openHomePage(){
+    homePage.style.display = 'flex'
+    scrapePage.style.display = 'none'
+    supplierPage.style.display = 'none'
+    requestPage.style.display = 'none'
+}
+
+scrapePageButton.addEventListener('click', openScrapePage)
+supplierPageButton.addEventListener('click', openSupplierPage)
+requestPageButton.addEventListener('click', openRequestPage)
+
+scrapeHomeButton.addEventListener('click', openHomePage)
+supplierHomeButton.addEventListener('click', openHomePage)
+requestHomeButton.addEventListener('click', openHomePage)
+
+
+
+
+
